c552b0f5a5623ac758bcd13b78b7cb7b2d8473b6
Ying Zhou <ybzhou@thoughtworks.com>
2018-04-14 13:15:57 +0800

    finish scafflold and eureka service

---
+++ products/src/main/java/com/thoughtworks/felix/products/ProductsApplication.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.felix.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApplication.class, args);
+	}
+}
